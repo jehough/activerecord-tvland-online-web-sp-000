b5d22aa087b9fc668ed84acdb0300c01892b7327
Jim Hough <teachlife@outlook.com>
2019-06-04 01:32:43 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_columns_to_songs.rb
+++ db/migrate/007_add_columns_to_songs.rb
@@ -1,2 +1,7 @@
 class AddColumnsToSongs < ActiveRecord::Migration[5.1]
-  
+  def change
+    add_column :songs, :day, :string
+    add_column :songs, :season, :string
+    add_column :songs, :genre, :string
+  end
+end
